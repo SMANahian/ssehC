6d23208e3212c8dee4780432b53226644adb1c6a
SMANahian <smanahian123@gmail.com>
2024-09-07 20:42:52 +0600

    Sliging, non-sliding pieces move + castling

--- src/genmove.c
+++ src/genmove.c
@@ -12,6 +12,24 @@ int LoopNonSlidePiece[6] = { wN, wK, 0, bN, bK, 0 };
 int LoopSlideIndex[2] = { 0, 4 };
 int LoopNonSlideIndex[2] = { 0, 3 };
 
+int PieceDir[13][8] = {
+    { 0, 0, 0, 0, 0, 0, 0, 0 },
+    { 0, 0, 0, 0, 0, 0, 0, 0 },
+    { -8, -19, -21, -12, 8, 19, 21, 12 },
+    { -9, -11, 11, 9, 0, 0, 0, 0 },
+    { -1, -10, 1, 10, 0, 0, 0, 0 },
+    { -1, -10, 1, 10, -9, -11, 11, 9 },
+    { -1, -10, 1, 10, -9, -11, 11, 9 },
+    { 0, 0, 0, 0, 0, 0, 0, 0 },
+    { -8, -19, -21, -12, 8, 19, 21, 12 },
+    { -9, -11, 11, 9, 0, 0, 0, 0 },
+    { -1, -10, 1, 10, 0, 0, 0, 0 },
+    { -1, -10, 1, 10, -9, -11, 11, 9 },
+    { -1, -10, 1, 10, -9, -11, 11, 9 }
+};
+
+int NumDir[13] = { 0, 0, 8, 4, 4, 8, 8, 0, 8, 4, 4, 8, 8 };
+
 
 void AddNormalMove(const BOARD *pos, int move, MOVE_LIST *list) {
     list->moves[list->count].move = move;
@@ -106,6 +124,10 @@ void GenerateAllMoves(const BOARD *pos, MOVE_LIST *list) {
     int side = pos->side;
     int sq = 0; int t_sq = 0;
     int pceNum = 0;
+    int dir = 0;
+    int index = 0;
+    int pceIndex = 0;
+    int pceNumIndex = 0;
     
     if(side == WHITE) {
         for(pceNum = 0; pceNum < pos->pieceNum[wP]; ++pceNum) {
@@ -132,6 +154,24 @@ void GenerateAllMoves(const BOARD *pos, MOVE_LIST *list) {
                 }
             }
         } 
+
+        // castling
+        if(pos->castlePerm & WKCA) {
+            if(pos->pieces[F1] == EMPTY && pos->pieces[G1] == EMPTY) {
+                if(!SquareAttacked(E1, BLACK, pos) && !SquareAttacked(F1, BLACK, pos) && !SquareAttacked(G1, BLACK, pos)) {
+                    AddNormalMove(pos, MOVE_INT(E1, G1, EMPTY, EMPTY, MASK_CA), list);
+                }
+            }
+        }
+
+        if(pos->castlePerm & WQCA) {
+            if(pos->pieces[D1] == EMPTY && pos->pieces[C1] == EMPTY && pos->pieces[B1] == EMPTY) {
+                if(!SquareAttacked(E1, BLACK, pos) && !SquareAttacked(D1, BLACK, pos) && !SquareAttacked(C1, BLACK, pos)) {
+                    AddNormalMove(pos, MOVE_INT(E1, C1, EMPTY, EMPTY, MASK_CA), list);
+                }
+            }
+        }
+
     } else {
         for(pceNum = 0; pceNum < pos->pieceNum[bP]; ++pceNum) {
             sq = pos->pieceList[bP][pceNum];
@@ -157,8 +197,91 @@ void GenerateAllMoves(const BOARD *pos, MOVE_LIST *list) {
                 }
             }
         }
+
+        // castling
+        if(pos->castlePerm & BKCA) {
+            if(pos->pieces[F8] == EMPTY && pos->pieces[G8] == EMPTY) {
+                if(!SquareAttacked(E8, WHITE, pos) && !SquareAttacked(F8, WHITE, pos) && !SquareAttacked(G8, WHITE, pos)) {
+                    AddNormalMove(pos, MOVE_INT(E8, G8, EMPTY, EMPTY, MASK_CA), list);
+                }
+            }
+        }
+
+        if(pos->castlePerm & BQCA) {
+            if(pos->pieces[D8] == EMPTY && pos->pieces[C8] == EMPTY && pos->pieces[B8] == EMPTY) {
+                if(!SquareAttacked(E8, WHITE, pos) && !SquareAttacked(D8, WHITE, pos) && !SquareAttacked(C8, WHITE, pos)) {
+                    AddNormalMove(pos, MOVE_INT(E8, C8, EMPTY, EMPTY, MASK_CA), list);
+                }
+            }
+        }
+
     }
 
+
+    // Sliding pieces
+    pceIndex = LoopSlideIndex[side];
+    pce = LoopSlidePiece[pceIndex++];
+
+    while(pce != 0) {
+        ASSERT(PieceValid(pce));
+
+        for(pceNum = 0; pceNum < pos->pieceNum[pce]; ++pceNum) {
+            sq = pos->pieceList[pce][pceNum];
+            ASSERT(SquareOnBoard(sq));
+
+            for(index = 0; index < NumDir[pce]; ++index) {
+                dir = PieceDir[pce][index];
+                t_sq = sq + dir;
+
+                while(!SQOFFBOARD(t_sq)) {
+                    if(pos->pieces[t_sq] != EMPTY) {
+                        if(PieceColor[pos->pieces[t_sq]] != side) {
+                            AddCaptureMove(pos, MOVE_INT(sq, t_sq, pos->pieces[t_sq], EMPTY, 0), list);
+                        }
+                        break;
+                    }
+                    AddNormalMove(pos, MOVE_INT(sq, t_sq, EMPTY, EMPTY, 0), list);
+                    t_sq += dir;
+                }
+            }
+
+        }
+        
+        pce = LoopSlidePiece[pceIndex++];
+    }
+
+    // Non-sliding pieces
+    pceIndex = LoopNonSlideIndex[side];
+    pce = LoopNonSlidePiece[pceIndex++];
+
+    while(pce != 0) {
+        ASSERT(PieceValid(pce));
+
+        for(pceNum = 0; pceNum < pos->pieceNum[pce]; ++pceNum) {
+            sq = pos->pieceList[pce][pceNum];
+            ASSERT(SquareOnBoard(sq));
+
+            for(index = 0; index < NumDir[pce]; ++index) {
+                dir = PieceDir[pce][index];
+                t_sq = sq + dir;
+
+                if(SQOFFBOARD(t_sq)) {
+                    continue;
+                }
+
+                if(pos->pieces[t_sq] != EMPTY) {
+                    if(PieceColor[pos->pieces[t_sq]] != side) {
+                        AddCaptureMove(pos, MOVE_INT(sq, t_sq, pos->pieces[t_sq], EMPTY, 0), list);
+                    }
+                } else {
+                    AddNormalMove(pos, MOVE_INT(sq, t_sq, EMPTY, EMPTY, 0), list);
+                }
+            }
+
+        }
+        
+        pce = LoopNonSlidePiece[pceIndex++];
+    }
     
 
 }
