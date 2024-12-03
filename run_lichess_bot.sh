6db70222d40cef9fffca2bdcaa48a4f1bc0a144e
SMANahian <smanahian123@gmail.com>
2024-12-03 17:35:19 +0600

    Final for heroku ig

--- run_lichess_bot.sh
+++ run_lichess_bot.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
 # Compile the engine
-make
+gcc src/sseh.c \
+    src/init.c \
+    src/bitboard.c \
+    src/hashkey.c \
+    src/board.c \
+    src/data.c \
+    src/attack.c \
+    src/io.c \
+    src/genmove.c \
+    src/validate.c \
+    src/makemove.c \
+    src/perft.c \
+    src/misc.c \
+    src/search.c \
+    src/pvtable.c \
+    src/evaluate.c \
+    src/uci.c \
+    src/xboard.c \
+    src/polybook.c \
+    src/polykeys.c \
+    -o ssehc \
+    -O3 -Wall -Wextra -Wpedantic -Wno-unused-result -Wno-unused-variable -Wno-unused-parameter -Wno-unused-function -Wno-unused-but-set-variable -Wno-unused-value -Wno-unused-label -Wno-unused-label -Wno-unused-local-typedefs
 
 # Put the engine in the engines folder
 cp ssehc lichess-bot/engines/
