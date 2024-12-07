ff1606e0b62d64335301b3637131ad0718be9c5d
SMANahian <smanahian123@gmail.com>
2024-12-08 00:26:00 +0600

    Heroku deployment: First try

--- run_lichess_bot.sh
+++ run_lichess_bot.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Compile the engine
-make
-
-# Put the engine in the engines folder
+# Copy the pre-built engine to the engines folder
 cp ssehc lichess-bot/engines/
 
 # Copy the default config file
@@ -14,11 +11,11 @@ cd lichess-bot
 
 # Create a virtual environment and install the requirements
 python3 -m venv venv
-virtualenv venv -p python3
 source ./venv/bin/activate
 python3 -m pip install -r requirements.txt
 
 # Inject LICHESS_TOKEN into config.yml
 sed -i "s|\${LICHESS_TOKEN}|$LICHESS_TOKEN|g" config.yml
 
+# Run the bot
 python3 lichess-bot.py -u
